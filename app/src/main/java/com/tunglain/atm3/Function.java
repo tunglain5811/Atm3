b94ae9d5e97ac0c8331040dfc48fc982f68a5acd
tunglain2 <tunglain2@gmail.com>
2019-11-23 16:25:40 +0800

    recyclerview 顯示文字+圖片功能

--- app/src/main/java/com/tunglain/atm3/Function.java
+++ app/src/main/java/com/tunglain/atm3/Function.java
@@ -2,6 +2,7 @@ package com.tunglain.atm3;
 
 public class Function {
     String name;
+    int icon;
 
     public Function() {
     }
@@ -10,11 +11,24 @@ public class Function {
         this.name = name;
     }
 
+    public Function(String name, int icon) {
+        this.name = name;
+        this.icon = icon;
+    }
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getIcon() {
+        return icon;
+    }
+
+    public void setIcon(int icon) {
+        this.icon = icon;
     }
 }
--- app/src/main/java/com/tunglain/atm3/MainActivity.java
+++ app/src/main/java/com/tunglain/atm3/MainActivity.java
@@ -6,20 +6,32 @@ import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_LOGIN = 100;
+    private static final String TAG = MainActivity.class.getSimpleName();
     private boolean logon = false;
+    private List<Function> functions;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,12 +54,83 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        setupRecyclerView();
+    }
+
+    private void setupRecyclerView() {
+        String[] functs = getResources().getStringArray(R.array.functions);
+        functions = new ArrayList<>();
+        functions.add(new Function(functs[0],R.drawable.func_transaction));
+        functions.add(new Function(functs[1],R.drawable.func_balance));
+        functions.add(new Function(functs[2],R.drawable.func_finance));
+        functions.add(new Function(functs[3],R.drawable.func_contacts));
+        functions.add(new Function(functs[4],R.drawable.func_exit));
+
         RecyclerView recycler = findViewById(R.id.recycler);
         recycler.setHasFixedSize(true);
-        recycler.setLayoutManager(new LinearLayoutManager(this));
-        FunctionAdapter adapter = new FunctionAdapter(this);
+//        recycler.setLayoutManager(new LinearLayoutManager(this));
+//        FunctionAdapter adapter = new FunctionAdapter(this);
+        recycler.setLayoutManager(new GridLayoutManager(this,3));
+        IconAdapter adapter = new IconAdapter();
         recycler.setAdapter(adapter);
     }
+
+    public class IconAdapter extends RecyclerView.Adapter<IconAdapter.IconHolder>{
+        @NonNull
+        @Override
+        public IconHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = getLayoutInflater()
+                    .inflate(R.layout.item_icon,parent,false);
+            return new IconHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull IconHolder holder, int position) {
+            final Function function = functions.get(position);
+            holder.nameText.setText(function.getName());
+            holder.iconImage.setImageResource(function.getIcon());
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    itemClicked(function);
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return functions.size();
+        }
+
+        public class IconHolder extends RecyclerView.ViewHolder{
+
+            ImageView iconImage;
+            TextView nameText;
+            public IconHolder(@NonNull View itemView) {
+                super(itemView);
+                iconImage = itemView.findViewById(R.id.item_icon);
+                nameText = itemView.findViewById(R.id.item_name);
+            }
+        }
+    }
+
+    private void itemClicked(Function function) {
+        Log.d(TAG, "itemClicked: " + function.getName());
+        switch (function.getIcon()){
+            case R.drawable.func_transaction:
+                break;
+            case R.drawable.func_balance:
+                break;
+            case R.drawable.func_finance:
+                break;
+            case R.drawable.func_contacts:
+                break;
+            case R.drawable.func_exit:
+                finish();
+                break;
+        }
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
